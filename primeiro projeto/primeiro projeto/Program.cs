465728266039f4b464ddf1dae20bd5d1a7fcd905
Rubens Santo <r4454406@gmail.com>
2023-06-19 18:39:02 -0300

    Adicionado projeto

---
+++ primeiro projeto/primeiro projeto/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace projeto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Olá Mundo!");
+        }
+    }
+}
